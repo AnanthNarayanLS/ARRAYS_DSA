60fe21e49db55a3f818e6bb261f622251e09060a
Ananth <ananthnarayan71@gmail.com>
2021-11-09 18:24:49 +0530

    problems on arrays

---
+++ 22.best_time_stock.py
@@ -0,0 +1,14 @@
+
+def maxprofit(arr):
+    left , right = 0 ,1
+    max_profit = 0
+
+    while right < len(arr):
+        if arr[left] < arr[right]:
+            profit = arr[right] - arr[left]
+            max_profit = max(profit , max_profit)
+        else:
+            left = right
+        right += 1
+
+    return max_profit
