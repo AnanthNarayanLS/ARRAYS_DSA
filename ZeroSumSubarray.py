b4e371e4c842a83e7bb01d562bcaafe516d47daa
Ananth <ananthnarayan71@gmail.com>
2021-11-17 18:29:10 +0530

    problems on arrays

---
+++ ZeroSumSubarray.py
@@ -0,0 +1,28 @@
+
+from array import *
+
+n = int(input("enter the number of values : "))
+arr = array('i' , [])
+for i in range(n):
+    temp = int(input("enter the array value : "))
+    arr.append(temp)
+
+print(f"your array is : {arr}")
+
+def zerosumsubarray(arr , n):
+    sum = 0
+    s = set()
+
+    for i in range(n):
+        sum += arr[i]
+
+        if sum == 0 or sum in s:
+            return True
+        s.add(sum)
+    return False
+
+print(zerosumsubarray(arr , n))
+if zerosumsubarray(arr , n) == True:
+    print("found a subarray with 0 sum")
+else:
+    print("no such sub array exists")
